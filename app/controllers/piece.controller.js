884a287bcbb2c068bae06ea29c7514aff0ecce54
stevenGrz13 <razafimahefasteven130102@gmail.com>
2025-03-28 10:52:20 +0300

    refactorisation fonctions et crud piece

--- app/controllers/piece.controller.js
+++ app/controllers/piece.controller.js
@@ -1,61 +1,28 @@
-const pieceService = require('../services/piece.service');
+const pieceService = require("../services/piece.service");
 const CrudController = require("../core/controllers/crud.controller");
+const ApiResponse = require("../core/response.model");
 
 class PieceController extends CrudController {
-    constructor() {
-        super(pieceService);
-    }
+  constructor() {
+    super(pieceService);
+  }
 
-    async findPieceRupture(req, res) {
-        try {
-            const piece = await pieceService.findEmptyPiece();
-            res.status(201).json(piece);
-        } catch (error) {
-            res.status(500).json({ message: "Erreur lors de la recherche des pieces en rupture de stock", error: error.message });
-        }
+  async findPieceRupture(req, res) {
+    try {
+      const piece = await pieceService.findEmptyPiece();
+      new ApiResponse(
+        200,
+        piece,
+        "Recuperation des pieces en rupture avec succes"
+      ).send(res);
+    } catch (error) {
+      new ApiResponse(
+        500,
+        null,
+        "Erreur lors de la récupération des pieces en rupture"
+      ).send(res);
     }
-
-    async createPiece(req, res) {
-        try {
-            const piece = await pieceService.createPiece(req.body);
-            res.status(201).json(piece);
-        } catch (error) {
-            res.status(500).json({ message: "Erreur lors de la création de la pièce", error: error.message });
-        }
-    }
-
-    async getPieces(req, res) {
-        try {
-            const pieces = await pieceService.getPieces();
-            res.status(200).json(pieces);
-        } catch (error) {
-            res.status(500).json({ message: "Erreur lors de la récupération des pièces", error: error.message });
-        }
-    }
-
-    async updatePiece(req, res) {
-        try {
-            const updatedPiece = await pieceService.updatePiece(req.params.id, req.body);
-            if (!updatedPiece) {
-                return res.status(404).json({ message: "Pièce non trouvée" });
-            }
-            res.status(200).json(updatedPiece);
-        } catch (error) {
-            res.status(500).json({ message: "Erreur lors de la mise à jour de la pièce", error: error.message });
-        }
-    }
-
-    async deletePiece(req, res) {
-        try {
-            const deletedPiece = await pieceService.deletePiece(req.params.id);
-            if (!deletedPiece) {
-                return res.status(404).json({ message: "Pièce non trouvée" });
-            }
-            res.status(200).json({ message: "Pièce supprimée avec succès" });
-        } catch (error) {
-            res.status(500).json({ message: "Erreur lors de la suppression de la pièce", error: error.message });
-        }
-    }
+  }
 }
 
 module.exports = new PieceController();
--- app/routes/piece.routes.js
+++ app/routes/piece.routes.js
@@ -2,10 +2,10 @@ const express = require('express');
 const router = express.Router();
 const pieceController = require('../controllers/piece.controller');
 
-router.post('/', pieceController.createPiece);
-router.get('/', pieceController.getPieces);
-router.put('/:id', pieceController.updatePiece);
-router.delete('/:id', pieceController.deletePiece);
+router.post('/', pieceController.create.bind(pieceController));
+router.get('/', pieceController.getAllPaginate.bind(pieceController));
+router.put('/:id', pieceController.update.bind(pieceController));
+router.delete('/:id', pieceController.delete.bind(pieceController));
 
 router.get('/findPieceRupture', pieceController.findPieceRupture);
 
