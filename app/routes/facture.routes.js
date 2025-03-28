1b258e3a566882807fb561bca940f451d5a66772
stevenGrz13 <razafimahefasteven130102@gmail.com>
2025-03-28 10:22:46 +0300

    refactorisation crud facture

--- app/routes/facture.routes.js
+++ app/routes/facture.routes.js
@@ -2,11 +2,9 @@ const express = require('express');
 const router = express.Router();
 const factureController = require('../controllers/facture.controller');
 
-router.post('/', factureController.createFacture);
-router.get('/', factureController.getFactures);
-router.put('/:id', factureController.updateFacture);
-router.delete('/:id', factureController.deleteFacture);
-
-router.get('/findFactureRupture', factureController.findFactureRupture);
+router.post('/', factureController.create.bind(factureController));
+router.get('/', factureController.getAllPaginate.bind(factureController));
+router.put('/:id', factureController.update.bind(factureController));
+router.delete('/:id', factureController.delete.bind(factureController));
 
 module.exports = router;
--- app/services/facture.service.js
+++ app/services/facture.service.js
@@ -1,36 +1,10 @@
+const CrudService = require('../core/services/crud.service');
 const Facture = require('../models/facture.model');
-const logger = require('../../utils/logger');
 
-exports.findEmptyFacture = async (factureData) => {
-    logger.info(`Création d'une facture avec les données: ${JSON.stringify(factureData)}`);
-    const liste = await Facture.find({ quantite: 0 });
-    return {
-        liste: liste,
-        nombre: liste.length
-    };
-};
+class FactureService extends CrudService{
+  constructor(){
+    super(Facture);
+  }
+}
 
-// Créer une facture
-exports.createFacture = async (factureData) => {
-    logger.info(`Création d'une facture avec les données: ${JSON.stringify(factureData)}`);
-    const facture = new Facture(factureData);
-    return await facture.save();
-};
-
-// Lire toutes les factures
-exports.getFactures = async () => {
-    logger.info('Récupération de toutes les factures');
-    return Facture.find();
-};
-
-// Mettre à jour une facture
-exports.updateFacture = async (id, factureData) => {
-    logger.info(`Mise à jour de la facture avec ID: ${id}`);
-    return Facture.findByIdAndUpdate(id, factureData, { new: true });
-};
-
-// Supprimer une facture
-exports.deleteFacture = async (id) => {
-    logger.info(`Suppression de la facture avec ID: ${id}...`);
-    return Facture.findByIdAndDelete(id);
-};
+module.exports = new FactureService();
